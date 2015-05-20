b12b443892af1dd7090350fc8d611686d5384cdf
logan dougher <logan.dougher@gmail.com>
2015-05-20 14:58:49 -0500

    movie login - rough

---
+++ src/js/main.js
@@ -0,0 +1,116 @@
+var FIREBASE_URL = 'https://c9-demo.firebaseio.com';
+var fb = new Firebase(FIREBASE_URL);
+
+$('.onTempPassword form').submit(function () {
+  var email = fb.getAuth().password.email;
+  var oldPw = $('.onTempPassword input:nth-child(1)').val();
+  var newPw = $('.onTempPassword input:nth-child(2)').val();
+
+  fb.changePassword({
+    email: email,
+    oldPassword: oldPw,
+    newPassword: newPw
+  }, function(err) {
+    if (err) {
+      alert(err.toString());
+    } else {
+      fb.unauth();
+    }
+  });
+
+  event.preventDefault();
+})
+
+$('.doResetPassword').click(function () {
+  var email = $('.onLoggedOut input[type="email"]').val();
+
+  fb.resetPassword({
+    email: email
+  }, function (err) {
+    if (err) {
+      alert(err.toString());
+    } else {
+      alert('Check your email!');
+    }
+  });
+});
+
+$('.doLogout').click(function () {
+  fb.unauth();
+})
+
+$('.doRegister').click(function () {
+  var email = $('.onLoggedOut input[type="email"]').val();
+  var password = $('.onLoggedOut input[type="password"]').val();
+
+  fb.createUser({
+    email: email,
+    password: password
+  }, function (err, userData) {
+    if (err) {
+      alert(err.toString());
+    } else {
+      doLogin(email, password);
+    }
+  });
+
+  event.preventDefault();
+});
+
+$('.onLoggedOut form').submit(function () {
+  var email = $('.onLoggedOut input[type="email"]').val();
+  var password = $('.onLoggedOut input[type="password"]').val();
+
+  doLogin(email, password);
+  event.preventDefault();
+});
+
+function clearLoginForm () {
+  $('input[type="email"]').val('');
+  $('input[type="password"]').val('');
+}
+
+function saveAuthData (authData) {
+  $.ajax({
+    method: 'PUT',
+    url: `${FIREBASE_URL}/users/${authData.uid}/profile.json`,
+    data: JSON.stringify(authData)
+  });
+}
+
+function doLogin (email, password, cb) {
+  fb.authWithPassword({
+    email: email,
+    password: password
+  }, function (err, authData) {
+    if (err) {
+      alert(err.toString());
+    } else {
+      saveAuthData(authData);
+      typeof cb === 'function' && cb(authData);
+    }
+  });
+}
+
+fb.onAuth(function (authData) {
+  var onLoggedOut = $('.onLoggedOut');
+  var onLoggedIn = $('.onLoggedIn');
+  var onTempPassword = $('.onTempPassword');
+
+  if (authData && authData.password.isTemporaryPassword) {
+    onTempPassword.removeClass('hidden');
+    onLoggedIn.addClass('hidden');
+    onLoggedOut.addClass('hidden');
+  } else if (authData) {
+    onLoggedIn.removeClass('hidden');
+    onLoggedOut.addClass('hidden');
+    onTempPassword.addClass('hidden');
+    $('.onLoggedIn h1').text(`Hello ${authData.password.email}`);
+  } else {
+    onLoggedOut.removeClass('hidden');
+    onLoggedIn.addClass('hidden');
+    onTempPassword.addClass('hidden');
+  }
+
+  clearLoginForm();
+});
